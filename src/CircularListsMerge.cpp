c295c6e4b188bb8d64bd6b95d549132178e9886f
aravindballa <aravindballa@missionrnd.com>
2016-04-02 21:00:16 +0530

    Submit Commit

--- src/CircularListsMerge.cpp
+++ src/CircularListsMerge.cpp
@@ -34,7 +34,7 @@ struct node{
 };
 int merge_circularlists(struct node **head1, struct node **head2){
 	//Returns Length of merged Sorted circular SLL and also points *head1 to final SLL .
-	if (head1==NULL || head2 == NULL)
+	if (*head1==NULL || *head2 == NULL)
 		return -1;
 
 	int length = 0;
